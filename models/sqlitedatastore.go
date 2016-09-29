cd21e0c63d64c0e5ca2e9bf724fde5765b904c04
Thomas Bellembois <thomas.bellembois@clermont.in2p3.fr>
2016-09-29 12:22:17 +0200

    gobkm: typo

--- models/sqlitedatastore.go
+++ models/sqlitedatastore.go
@@ -3,10 +3,9 @@ package models
 import (
 	"database/sql"
 
-	"github.com/tbellembois/gobkm/types"
-
 	log "github.com/Sirupsen/logrus"
 	_ "github.com/mattn/go-sqlite3" // register sqlite3 driver
+	"github.com/tbellembois/gobkm/types"
 )
 
 const (
@@ -23,7 +22,6 @@ type SQLiteDataStore struct {
 // NewDBstore returns a database connection to the given dataSourceName
 // ie. a path to the sqlite database file.
 func NewDBstore(dataSourceName string) (*SQLiteDataStore, error) {
-
 	log.WithFields(log.Fields{
 		"dataSourceName": dataSourceName,
 	}).Debug("NewDBstore:params")
@@ -39,63 +37,57 @@ func NewDBstore(dataSourceName string) (*SQLiteDataStore, error) {
 		}).Error("NewDBstore:error opening the database")
 		return nil, err
 	}
-
 	return &SQLiteDataStore{db, nil}, nil
-
 }
 
 // FlushErrors returns the last DB errors and flushes it.
 func (db *SQLiteDataStore) FlushErrors() error {
-
+	// Saving the last thrown error.
 	lastError := db.err
+	// Resetting the error.
 	db.err = nil
+	// Returning the last error.
 	return lastError
-
 }
 
 // CreateDatabase creates the database tables.
 func (db *SQLiteDataStore) CreateDatabase() {
-
 	log.Info("Creating database")
-
-	var count int
-
+	// Activate the foreign keys feature.
 	if _, db.err = db.Exec("PRAGMA foreign_keys = ON"); db.err != nil {
 		log.Error("CreateDatabase: error executing the PRAGMA request")
 		return
 	}
 
+	// Tables creation if needed.
 	if _, db.err = db.Exec("CREATE TABLE IF NOT EXISTS folder ( id integer PRIMARY KEY, title string NOT NULL, parentFolderId integer, nbChildrenFolders integer, FOREIGN KEY (parentFolderId) references folder(id) ON DELETE CASCADE)"); db.err != nil {
 		log.Error("CreateDatabase: error executing the CREATE TABLE request for table bookmark")
 		return
 	}
-
 	if _, db.err = db.Exec("CREATE TABLE IF NOT EXISTS bookmark ( id integer PRIMARY KEY, title string NOT NULL, url string NOT NULL, favicon string, starred integer, folderId integer, FOREIGN KEY (folderId) references folder(id) ON DELETE CASCADE)"); db.err != nil {
 		log.Error("CreateDatabase: error executing the CREATE TABLE request for table bookmark")
 		return
 	}
-
+	// Looking for folders.
+	var count int
 	if db.err = db.QueryRow("SELECT COUNT(*) as count FROM folder").Scan(&count); db.err != nil {
 		log.Error("CreateDatabase: error executing the SELECT COUNT(*) request for table folder")
 		return
 	}
-
+	// Inserting the / folder if not present.
 	if count > 0 {
+		log.Info("CreateDatabase: folder table not empty, leaving")
 		return
 	}
-
 	if _, db.err = db.Exec("INSERT INTO folder(id, title) values(\"1\", \"/\")"); db.err != nil {
 		log.Error("CreateDatabase: error inserting the root folder")
 		return
 	}
-
 }
 
 // PopulateDatabase populate the database with sample folders and bookmarks.
 func (db *SQLiteDataStore) PopulateDatabase() {
-
 	log.Info("Populating database")
-
 	// Leaving silently on past errors...
 	if db.err != nil {
 		return
@@ -107,7 +99,7 @@ func (db *SQLiteDataStore) PopulateDatabase() {
 		count     int
 	)
 
-	// Leaving if database already populated.
+	// Leaving if database is already populated.
 	if db.err = db.QueryRow("SELECT COUNT(*) as count FROM folder").Scan(&count); db.err != nil || count > 1 {
 		log.Info("Database not empty, leaving")
 		return
@@ -115,14 +107,11 @@ func (db *SQLiteDataStore) PopulateDatabase() {
 
 	// Getting the root folder.
 	folderRoot := db.GetFolder(1)
-
 	// Creating new sample folders.
 	folder1 := types.Folder{Id: 1, Title: "IT", Parent: folderRoot}
 	folder2 := types.Folder{Id: 2, Title: "Development", Parent: &folder1}
-
 	// Creating new sample bookmark.
 	bookmark1 := types.Bookmark{Id: 1, Title: "GoLang", Starred: true, URL: "https://golang.org/", Favicon: "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAb9JREFUOI3tkj9oU1EUh797c3lgjA4xL61FX0yhMQqmW5QgFim4+GcyQ3Hp1MlBqFIyOGUobRScnYoQikNA0Ao6WJS2UIdiK7SUVGtfIZg0iMSA+Iy5Dg9fGnyLu2e6nHPu9zv3/K7QWuMXjfqebjQbOM5PIuEjHI6Ywq9P/TlUdm09+3KeNxtlAHbLWzTrNeTBQxjhHuLHohrgwqkBRi5dpO+4JQDEh80NfePOXaIDJ3FigximBUAyk+5SOvFphR/tNovvyzg769TKmxQLecS5a9d1dOQ2zp7N6bjF1PAZlJKMv1hFpVxIa+0t96+cBWD82TLr2zaGaVGbvYcEqLx+gmFajKZiqANBeo/2MZcb89RHUzEAeiNh5nJjGKZF9VUJAFks5FGVrc7IuuW7VH518slMGlHdpljII/sTSW+7j5ohEIrP9S9cnnxIaShOaSjOzNoOBNz81ceLHqg/kRRqv0ggGGLCdm3t+fqRmZtZ15HKEhN2Go1ABUO06VjfBdDSLQS0IFNd4fytSQAWHuR4B8gW7lWJP8B7rtA8zU7zfH4V8f0brew0ou37j/wBHigx2D2d/LvHJ/Vv8R8AvwHjjZMncK4ImgAAAABJRU5ErkJggg==", Folder: &folder2}
-
 	folders = append(folders, &folder1, &folder2)
 	bookmarks = append(bookmarks, &bookmark1)
 
@@ -133,16 +122,13 @@ func (db *SQLiteDataStore) PopulateDatabase() {
 	for _, bkm := range bookmarks {
 		db.SaveBookmark(bkm)
 	}
-
 }
 
-// GetBookmark returns a Bookmark instance with the given id
+// GetBookmark returns a Bookmark instance with the given id.
 func (db *SQLiteDataStore) GetBookmark(id int) *types.Bookmark {
-
 	log.WithFields(log.Fields{
 		"id": id,
 	}).Debug("GetBookmark")
-
 	// Leaving silently on past errors...
 	if db.err != nil {
 		return nil
@@ -153,24 +139,20 @@ func (db *SQLiteDataStore) GetBookmark(id int) *types.Bookmark {
 		starred  sql.NullInt64
 	)
 
+	// Querying the bookmark.
 	bkm := new(types.Bookmark)
-
 	db.err = db.QueryRow("SELECT id, title, url, favicon, starred, folderId FROM bookmark WHERE id=?", id).Scan(&bkm.Id, &bkm.Title, &bkm.URL, &bkm.Favicon, &starred, &folderID)
-
 	switch {
-
 	case db.err == sql.ErrNoRows:
 		log.WithFields(log.Fields{
 			"id": id,
 		}).Debug("GetBookmark:no bookmark with that ID")
 		return nil
-
 	case db.err != nil:
 		log.WithFields(log.Fields{
 			"err": db.err,
 		}).Error("GetBookmark:SELECT query error")
 		return nil
-
 	default:
 		log.WithFields(log.Fields{
 			"Id":       bkm.Id,
@@ -178,82 +160,65 @@ func (db *SQLiteDataStore) GetBookmark(id int) *types.Bookmark {
 			"folderId": folderID,
 			"Favicon":  bkm.Favicon,
 		}).Debug("GetBookmark:bookmark found")
-
+		// Starred bookmark ?
 		if int(starred.Int64) != 0 {
 			bkm.Starred = true
 		}
-
-		// retrieving the parent
+		// Retrieving the parent folder if it is not the root (/).
 		if folderID.Int64 != 0 {
-
 			bkm.Folder = db.GetFolder(int(folderID.Int64))
-
 			if db.err != nil {
 				log.WithFields(log.Fields{
 					"err": db.err,
 				}).Error("GetBookmark:parent Folder retrieving error")
 				return nil
 			}
-
 		}
-
 	}
-
 	return bkm
 }
 
-// GetFolder returns a Folder instance with the given id
+// GetFolder returns a Folder instance with the given id.
 func (db *SQLiteDataStore) GetFolder(id int) *types.Folder {
-
 	log.WithFields(log.Fields{
 		"id": id,
 	}).Debug("GetFolder")
-
 	// Leaving silently on past errors...
 	if db.err != nil || id == 0 {
 		return nil
 	}
 
+	// Querying the folder.
 	var parentFldID sql.NullInt64
 	fld := new(types.Folder)
-
 	db.err = db.QueryRow("SELECT id, title, parentFolderId FROM folder WHERE id=?", id).Scan(&fld.Id, &fld.Title, &parentFldID)
-
 	switch {
-
 	case db.err == sql.ErrNoRows:
 		log.WithFields(log.Fields{
 			"id": id,
 		}).Debug("GetFolder:no folder with that ID")
 		return nil
-
 	case db.err != nil:
 		log.WithFields(log.Fields{
 			"err": db.err,
 		}).Error("GetFolder:SELECT query error")
 		return nil
-
 	default:
 		log.WithFields(log.Fields{
 			"Id":          fld.Id,
 			"Title":       fld.Title,
 			"parentFldId": parentFldID,
 		}).Debug("GetFolder:folder found")
-
 		// recursively retrieving the parents
 		if parentFldID.Int64 != 0 {
-
 			fld.Parent = db.GetFolder(int(parentFldID.Int64))
-
 		}
 	}
-
 	return fld
 }
 
 // GetStarredBookmarks returns the starred bookmarks
 func (db *SQLiteDataStore) GetStarredBookmarks() []*types.Bookmark {
-
 	// Leaving silently on past errors...
 	if db.err != nil {
 		return nil
